ba34e98131674847d8992aff6eef351a468e14c5
Nahid Ibn Ali <nahidibnali7@gmail.com>
2022-09-29 10:41:42 +0600

    added home and header component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const Home = () => {
+    return (
+        <div className='row'>
+            <div className="col-lg-9 py-4 bg-light">
+                <div className="container">
+                    <Header></Header>
+                </div>
+            </div>
+            <div className="col-lg-3 p-5">
+                
+            </div>
+        </div>
+    );
+};
+
+export default Home;
