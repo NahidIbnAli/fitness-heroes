f3cb1a3f011ea5250884fce4019d130f8b029f5f
Nahid Ibn Ali <nahidibnali7@gmail.com>
2022-09-29 23:13:55 +0600

    added toast to the site

--- src/components/ExerciseDetails/ExerciseDetails.js
+++ src/components/ExerciseDetails/ExerciseDetails.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ExerciseDetails = ({exercises, selectedBreak}) => {
     let total = 0;
     for(const exercise of exercises) {
         total+= exercise.time
     }
+    const notify = () => toast("Congratulations! You are done with your activity");
     return (
         <div className='py-5'>
             <h4 className='my-4'>Exercise Details</h4>
@@ -16,7 +19,8 @@ const ExerciseDetails = ({exercises, selectedBreak}) => {
                 <h5 className='m-0'>Break time</h5>
                 <p className='fs-5 text-muted m-0'>{selectedBreak}</p>
             </div>
-            <button className='btn btn-lg btn-warning w-100 mt-5 rounded-0'>Activity Completed</button>
+            <button onClick={notify} className='btn btn-lg btn-warning w-100 mt-5 rounded-0'>Activity Completed</button>
+            <ToastContainer />
         </div>
     );
 };
